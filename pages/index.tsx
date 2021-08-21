b238a7139faaa4c1a2406143395ef3bdcad8f3a3
Gustavo <ordazsgustavo@gmail.com>
2021-08-20 21:07:33 -0500

    Implement graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,31 @@
+import { request, gql } from 'graphql-request';
+import { useQuery } from 'react-query';
+
+const query = gql`
+  query {
+    characters {
+      info {
+        count
+      }
+      results {
+        name
+      }
+    }
+  }
+`;
+
+const endpoint = process.env.NEXT_PUBLIC_API_URL!;
+
+function useCharacters() {
+  return useQuery('characters', async () => {
+    const { characters } = await request(endpoint, query);
+
+    return characters.results;
+  });
+}
+
 export default function Index() {
+  const { data } = useCharacters();
+  console.log(data, endpoint);
   return <div>Hello</div>;
 }
